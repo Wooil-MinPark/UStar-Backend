9906c76120b8d25d0408c7415531a59dd750fe7c
Seang-G <mgw7730@gmail.com>
2024-08-25 19:51:35 +0900

    INIT

---
+++ src/main/java/com/wooil/ustar/UnderTheStarsApplication.java
@@ -0,0 +1,13 @@
+package com.wooil.ustar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderTheStarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnderTheStarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wooil/ustar/UnderTheStarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wooil.ustar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderTheStarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
