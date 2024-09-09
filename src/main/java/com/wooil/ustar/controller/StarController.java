f3a0ae9edc1c308b88f10da81e601c485737bdd6
민지원 <mgw7730@envalue.biz>
2024-09-09 11:03:57 +0900

    [ADD] PR Rule 테스트용 코드

--- src/main/java/com/wooil/ustar/controller/StarController.java
+++ src/main/java/com/wooil/ustar/controller/StarController.java
@@ -1,4 +1,16 @@
 package com.wooil.ustar.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class StarController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello";
+    }
 }
