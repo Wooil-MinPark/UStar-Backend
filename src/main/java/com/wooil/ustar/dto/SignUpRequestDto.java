a5bab54ee28a998227ce3c9abd34e99bf5639970
박기현 <kihyun1998@penta.co.kr>
2024-09-10 19:29:15 +0900

    [Add] 회원가입 request dto 추가

---
+++ src/main/java/com/wooil/ustar/dto/SignUpRequestDto.java
@@ -0,0 +1,26 @@
+package com.wooil.ustar.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpRequestDto {
+
+    @NotBlank(message = "ID is required")
+    private String id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    // 이렇게 사이즈 지정 가능
+    // @Size(min = 8, message = "Password must be at least 8 characters long")
+    @NotBlank(message = "Password is required")
+    private String password;
+}
