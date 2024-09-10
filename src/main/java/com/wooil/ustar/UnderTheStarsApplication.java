869a169aaaed1d70cd1f3f596a2808937ea2c5f1
박기현 <kihyun1998@penta.co.kr>
2024-09-10 19:28:21 +0900

    [Update] 시간 자동 생성을 위한 의존성 추가

--- src/main/java/com/wooil/ustar/UnderTheStarsApplication.java
+++ src/main/java/com/wooil/ustar/UnderTheStarsApplication.java
@@ -2,8 +2,10 @@ package com.wooil.ustar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UnderTheStarsApplication {
 
 	public static void main(String[] args) {
