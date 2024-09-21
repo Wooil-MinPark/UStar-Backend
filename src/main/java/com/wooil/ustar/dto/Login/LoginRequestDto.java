e778a9d8050475f9ba2f75d23f817cadaa3e079d
pentakihyun <71832733+kihyun1998@users.noreply.github.com>
2024-09-21 18:07:52 +0900

    [Edit] add @AllArgsConstructor for constructor

--- src/main/java/com/wooil/ustar/dto/Login/LoginRequestDto.java
+++ src/main/java/com/wooil/ustar/dto/Login/LoginRequestDto.java
@@ -1,10 +1,12 @@
 package com.wooil.ustar.dto.Login;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class LoginRequestDto {
     private String userId;
     private String password;
