cf2c207c0e80dfecc998f69f2639d398fdf125b4
Seang-G <mgw7730@gmail.com>
2024-09-27 20:31:43 +0900

    [FEAT] CORS 설정 추가 - 일단은 모두 허용

--- src/main/java/com/wooil/ustar/config/WebSecurityConfig.java
+++ src/main/java/com/wooil/ustar/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.wooil.ustar.Util.jwt.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,11 +13,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
 
     private final JwtFilter jwtFilter;
 
@@ -24,10 +27,12 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .authorizeHttpRequests((auth) -> auth.requestMatchers("/api/user/signup", "/api/user/login", "/api/auth/refresh").permitAll()
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/api/user/signup", "/api/user/login", "/api/auth/refresh").permitAll()
                         .anyRequest()
                         .authenticated()
                 )
@@ -37,8 +42,18 @@ public class WebSecurityConfig {
         return http.build();
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
+                .allowedOriginPatterns("*")  // 허용할 출처 지정
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메소드
+                .allowedHeaders("*")  // 모든 헤더 허용
+                .allowCredentials(true);  // 인증 정보 허용 (쿠키, 헤더 등)
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
