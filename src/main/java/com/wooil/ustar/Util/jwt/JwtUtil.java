6bad2827a6e43dde5e3f66f3e6129649c4c3166e
박기현 <kihyun1998@penta.co.kr>
2024-10-02 22:19:50 +0900

    Update JwtUtil.java

--- src/main/java/com/wooil/ustar/Util/jwt/JwtUtil.java
+++ src/main/java/com/wooil/ustar/Util/jwt/JwtUtil.java
@@ -8,27 +8,29 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
-import java.util.Date;
-
 @Component
 public class JwtUtil {
+
+    @Value("${jwt.secret}")
     private String secret;
 
-    @Value("${jwt.accessTokenValidity}")
+    @Value("${jwt.access-token-validity}")
     private long accessTokenValidity;
 
-    @Value("${jwt.refreshTokenValidity}")
+    @Value("${jwt.refresh-token-validity}")
     private long refreshTokenValidity;
 
     private Key key;
 
     @PostConstruct
     public void init() {
-        this.key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
 
     /*
@@ -39,11 +41,11 @@ public class JwtUtil {
     */
     public String generateAccessToken(String userEmail) {
         return Jwts.builder()
-                .setSubject(userEmail)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + accessTokenValidity))
-                .signWith(key)
-                .compact();
+            .setSubject(userEmail)
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + accessTokenValidity))
+            .signWith(key)
+            .compact();
     }
 
     /*
@@ -54,19 +56,19 @@ public class JwtUtil {
     */
     public String generateRefreshToken(String userEmail) {
         return Jwts.builder()
-                .setSubject(userEmail)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenValidity))
-                .signWith(key)
-                .compact();
+            .setSubject(userEmail)
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + refreshTokenValidity))
+            .signWith(key)
+            .compact();
     }
 
     public boolean validateToken(String token) {
         try {
             Jwts.parserBuilder()
-                    .setSigningKey(key)
-                    .build()
-                    .parseClaimsJws(token);
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
             return true;
         } catch (ExpiredJwtException e) {
             throw new CustomException(ErrorCode.TOKEN_002);
@@ -77,10 +79,10 @@ public class JwtUtil {
 
     public String getUsernameFromToken(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .getBody()
+            .getSubject();
     }
 }
