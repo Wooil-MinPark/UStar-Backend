0851141f4153234d25ab7d4a72f26515577602e1
박기현 <kihyun1998@penta.co.kr>
2024-10-03 11:24:26 +0900

    [Docs] 안쓰는 패키지 제거

--- src/test/java/com/wooil/ustar/service/UserServiceTest.java
+++ src/test/java/com/wooil/ustar/service/UserServiceTest.java
@@ -23,7 +23,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
