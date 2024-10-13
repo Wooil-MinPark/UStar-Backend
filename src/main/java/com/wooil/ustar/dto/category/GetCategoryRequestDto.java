c77f70b1aa9de4da7249d937c331ec457b1153cb
박기현 <kihyun1998@penta.co.kr>
2024-10-13 20:55:40 +0900

    [Add] get category dto 구현

---
+++ src/main/java/com/wooil/ustar/dto/category/GetCategoryRequestDto.java
@@ -0,0 +1,14 @@
+package com.wooil.ustar.dto.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class GetCategoryRequestDto {
+    private Long categoryUid;
+}
