818d09a542d736473b2f15a09b13b9295aab91fd
박기현 <kihyun1998@penta.co.kr>
2024-10-25 22:34:43 +0900

    [Update] signup dto 변수명 수정

--- src/main/java/com/wooil/ustar/dto/SignUpRequestDto.java
+++ src/main/java/com/wooil/ustar/dto/SignUpRequestDto.java
@@ -10,13 +10,13 @@ import lombok.*;
 @NoArgsConstructor
 public class SignUpRequestDto {
     @NotBlank(message = "Name is required")
-    private String name;
+    private String userName;
 
     @NotBlank(message = "Email is required")
-    private String email;
+    private String userEmail;
 
     // 이렇게 사이즈 지정 가능
     // @Size(min = 8, message = "Password must be at least 8 characters long")
     @NotBlank(message = "Password is required")
-    private String password;
+    private String userPassword;
 }
