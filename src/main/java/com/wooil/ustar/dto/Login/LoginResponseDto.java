ec233e37dc27e62f489c5f3d3c66b53b1ca06868
박기현 <kihyun1998@penta.co.kr>
2024-11-09 00:03:35 +0900

    [Update] getter 추가

--- src/main/java/com/wooil/ustar/dto/Login/LoginResponseDto.java
+++ src/main/java/com/wooil/ustar/dto/Login/LoginResponseDto.java
@@ -1,7 +1,9 @@
 package com.wooil.ustar.dto.Login;
 
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 public record LoginResponseDto(String accessToken, String refreshToken) {
 }
